409b254a54185b0f84218c13d2e2e427eb8df1a6
Martin Prot <martinprot@gmail.com>
2018-03-27 14:58:59 +0200

    Added a simple circle view ibdesigned

---
+++ mpkit/Classes/UI/UICircleView.swift
@@ -0,0 +1,20 @@
+//
+//  UICircleView.swift
+//  mpkit
+//
+//  Created by Martin Prot on 01/03/20178.
+//  Copyright © 2017 Appricot media. All rights reserved.
+//
+
+import UIKit
+
+//@IBDesignable
+
+final public class UICircleView: UIView {
+	
+	public override func layoutSubviews() {
+		super.layoutSubviews()
+		self.layer.cornerRadius = min(self.bounds.width, self.bounds.height)/2
+		self.clipsToBounds = false
+	}
+}
